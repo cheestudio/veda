2a939e84c7e03fb8414924a02d5d1bda030ad66f
Matt <matt@goodchee.com>
2020-02-04 16:55:03 -0800

    front end dev

--- wp-content/themes/veda-custom-theme/partials/breadcrumbs.php
+++ wp-content/themes/veda-custom-theme/partials/breadcrumbs.php
@@ -1,3 +1,10 @@
 <?php if ( function_exists('bcn_display') ) : ?>
-  <div class="breadcrumbs"><?php bcn_display(); ?></div>
+  <div class="breadcrumbs">
+
+    <?php bcn_display();
+
+    // If Single Post, show date
+    if ( get_post_type() == 'post' ) echo "<span class='post post-post current-item'>" . get_the_date() . "</span>"; ?>
+    
+    </div>
 <?php endif; ?>
